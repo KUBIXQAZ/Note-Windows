170742b7ceabc443c4b8de2109883f5d9e1b34d0
KUBIXQAZ <kubajozwik20027@gmail.com>
2023-06-20 21:10:01 +0200

    ssssssss

--- TaskViewPage.xaml.cs
+++ TaskViewPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -9,12 +10,16 @@ namespace Note
     public partial class TaskViewPage : Page
     {
         MainWindow.Task currTask;
+        string title;
+        string desc;
 
         public TaskViewPage(MainWindow.Task task)
         {
             InitializeComponent();
 
             currTask = task;
+            title = currTask.Title;
+            desc = currTask.Description;
         }
 
         private void BackEditB_Click(object sender, RoutedEventArgs e)
@@ -41,6 +46,58 @@ namespace Note
                     string deleteQuery = "DELETE FROM Tasks WHERE id = @id";
                     MySqlCommand command = new MySqlCommand(deleteQuery, connection);
                     command.Parameters.AddWithValue("@id", taskToDelete.Id);
+
+                    command.ExecuteNonQuery();
+
+                    NavigationService.Navigate(new MainPage());
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error saving tasks to the database: " + ex.Message);
+                }
+            }
+        }
+
+        private void TitleTask_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            checkChange();
+        }
+
+        private void DescTask_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            checkChange();
+        }
+
+        public void checkChange()
+        {
+            if (title != TitleTask.Text || desc != DescTask.Text)
+            {
+                SaveExitB.Visibility = Visibility.Visible;
+
+                if (TitleTask.Text == "" || DescTask.Text == "") SaveExitB.IsEnabled = false;
+                else SaveExitB.IsEnabled = true;
+            }
+            else
+            {
+                SaveExitB.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void SaveExitB_Click(object sender, RoutedEventArgs e)
+        {
+            using (MySqlConnection connection = new MySqlConnection(Settings.connection_string))
+            {
+                try
+                {
+                    connection.Open();
+
+                    MainWindow.Task taskToDelete = currTask;
+
+                    string updateQuery = "UPDATE Tasks SET Title = @title, Description = @desc WHERE id = @id;";
+                    MySqlCommand command = new MySqlCommand(updateQuery, connection);
+                    command.Parameters.AddWithValue("@id", taskToDelete.Id);
+                    command.Parameters.AddWithValue("@title", TitleTask.Text);
+                    command.Parameters.AddWithValue("@desc", DescTask.Text);
 
                     command.ExecuteNonQuery();
 
